96f7805963a94962906192ba2afc2221ad31476d
ig6268 <32320724+ig6268@users.noreply.github.com>
2018-01-16 23:40:33 +0900

    개인키 추가 완료, 복호화 완료,   사용예를 위한 버튼 생성

--- main.java
+++ main.java
@@ -13,6 +13,7 @@ public class main extends JFrame{
 	JLabel state;
 	JTextArea txt_area;
 	static int m;//평문 m
+	static int em;// 복호화된 m
 	static int c;// 암호문
 	static int e;// 공개키
 	static int d;// 개인키
@@ -32,13 +33,15 @@ public class main extends JFrame{
 		JButton btn1 = new JButton ("암호화");
 		JButton btn2 = new JButton ("복호화");
 		JButton btn3 = new JButton ("키생성");
+		JButton btn4 = new JButton ("예시");
 		JTextField tf1 = new JTextField(10);
 		
-		Btn_panel.add(new JLabel("평문값(2~50사이의 정수값)"));
+		Btn_panel.add(new JLabel("2~50사이의 정수값"));
 		Btn_panel.add(tf1);
 		Btn_panel.add(btn3);
 		Btn_panel.add(btn1);
 		Btn_panel.add(btn2);	
+		Btn_panel.add(btn4);
 		
 		btn1.addActionListener(new ActionListener() { // 암호화 버튼
 			//@Override
@@ -51,9 +54,9 @@ public class main extends JFrame{
 		btn2.addActionListener(new ActionListener() { //복호화 버튼
 			//@Override
 			public void actionPerformed(ActionEvent aa) {
-				
+				em = decode(c,d,m);
 				txt_area.append("---------------------------------------------" + "\n");
-				txt_area.append("평문 M = "+ m +"\n");
+				txt_area.append("복호화 M = "+ m +"\n");
 			}
 		});
 		btn3.addActionListener(new ActionListener() { //키 생성 버튼
@@ -73,7 +76,14 @@ public class main extends JFrame{
 					}
 			}
 		});
-
+		
+		btn4.addActionListener(new ActionListener() { // 사용예시 로그인 버튼
+			//@Override
+			public void actionPerformed(ActionEvent aa) {
+				start_Sub bb = new start_Sub(); // ex클래스의 start_Sub 매소드 실행
+			}
+		});
+		
 		// 프로그램 틀
 		//state = new JLabel(); 
 		//state.setText("RSA 키사용 암,복호화");
@@ -82,17 +92,14 @@ public class main extends JFrame{
 		add(txt_area, BorderLayout.CENTER);
 		add(Btn_panel, BorderLayout.SOUTH);
 		//setLayout(new GridLayout(2,0));
-		setSize(530, 400);
+		setSize(550, 400);
 		setVisible(true);
 	}
 	
 	public void create() { // 키 생성 함수
 		p = getRandomNum(2,100,true); // p,q를 생성할때 판별을 위해 t/f추가
 		q = getRandomNum(2,100,true);
-		if(p == q)
-		{
-			create();
-		}
+		
 		txt_area.append("p,q = "+p+" , "+q+"\n");
 		
 		n = (p * q);
@@ -107,9 +114,12 @@ public class main extends JFrame{
 		
 		//GCD(pi,e);
 		//txt_area.append("gcd("+pi+","+e+") = "+t+"\n");
-		 
+		
+		d = publickey(pi,e); 
+		txt_area.append("d값 = " + d + "\n");
+		
 		txt_area.append("공개키 값 = {"+e+","+n+"}\n");
-		//txt_area.append("개인키 값 = {"+d+","+n+"}\n");
+		txt_area.append("개인키 값 = {"+d+","+n+"}\n");
 	}
 	
 	//임의의 수 p와 q를 반환하는 메소드
@@ -199,13 +209,26 @@ public class main extends JFrame{
 		return e; 
 	}
 	
-	public static int decode(int a1, int a2, int a3)
+	public static int privatekey(int e, int pi) //개인키 계산
+	{
+		int b1 = 0;
+		
+		while((e*b1)%pi != 1) // e를b1번 곱해서 mod pi를 한 값이 1이 아닐때 b1을 증가시킨다.
+		{
+			b1++;
+			if(e*b1%pi == 1) // e를 b1번 곱해서 mod pi를 한 값이 1이면 b1의 값이 d가된다.
+				d = b1;
+				break;
+		}
+		return d;
+	}
+	public static int decode(int a1, int a2, int a3)//암,복호화 계산
 	{	
 		int result = a1;
 		
-		for(int i=1; i<a2; i++)
+		for(int i=1; i<a2; i++) // 지수(a2)만큼 곱한다.
 		{
-			result = (result * a1) % a3;
+			result = (result * a1) % a3; // result의 a1승을 계산 mod a3
 		}
 		return result;
 	}
